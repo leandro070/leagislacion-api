cd0d4f2dfa27d87b6067da9ced25ea51a858c781
Leandro Gutierrez <leandrogutierrez070@gmail.com>
2019-05-27 21:30:27 -0300

    ADD CORS

--- main.go
+++ main.go
@@ -26,6 +26,22 @@ func homePage(c *gin.Context) {
 	}
 }
 
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func AuthRequired() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
@@ -55,6 +71,7 @@ func handlerFunctions() {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.Use(CORS())
 
 	r.GET("/home", homePage)
 	r.POST("/newUser", user.CreateUserHandler)
